b81aef4e72371b8476f2649c252d35cd4d0e7b79
GugaGordeladze <gugagordeladze@yahoo.com>
2024-07-23 16:15:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function generateWiFiQRCode() {
-    const ssid = document.getElementById('ssid').value;
-    const password = document.getElementById('password').value;
-    const encryption = document.getElementById('encryption').value;
-
-    const wifiString = `WIFI:T:${encryption};S:${ssid};P:${password};;`;
-
-    QRCode.toDataURL(wifiString, function (err, url) {
-        if (err) console.error(err);
-
-        const img = document.createElement('img');
-        img.src = url;
-        const qrCodeContainer = document.getElementById('wifi-qr-code');
-        qrCodeContainer.innerHTML = '';
-        qrCodeContainer.appendChild(img);
-    });
-}
-
 function generateLinkQRCode() {
     const link = document.getElementById('link').value;
 
@@ -27,5 +9,20 @@ function generateLinkQRCode() {
         const qrCodeContainer = document.getElementById('link-qr-code');
         qrCodeContainer.innerHTML = '';
         qrCodeContainer.appendChild(img);
+
+        const downloadButton = document.getElementById('download-button');
+        downloadButton.style.display = 'block';
+        downloadButton.onclick = function() {
+            downloadQRCode(url);
+        };
     });
+}
+
+function downloadQRCode(url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'qr_code.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
